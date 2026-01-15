a7d3897e3b5fbe1985011d3947ca95c36841f37f
devschandel <max.carl2022@gmail.com>
2026-01-16 02:08:17 +0530

    stable js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -6,7 +6,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
-
